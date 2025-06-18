a02e45be59c3e0ccd12ea0c757f290cf91d02290
Mehdi Bedja <Younes1967.yy@gmail.com>
2025-06-19 00:51:07 +0100

    added multiple features

--- pest_detection_backend/pest_detection_backend/settings.py
+++ pest_detection_backend/pest_detection_backend/settings.py
@@ -129,7 +129,7 @@ WSGI_APPLICATION = "pest_detection_backend.wsgi.application"
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
         'USER': os.getenv('DB_USER', ''),
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
